3464e6722f679af263029dd73707dfd4fe15bfc5
dFarkhod <dfarkhod@gmail.com>
2020-02-07 17:47:03 +0800

    updated

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
-//const tf = require('@tensorflow/tfjs');
+async function PredictCelciusToFarhrenheit() {
 
-async function GetPrediction() {
-   
-    // Define a model for linear regression.
+    // chiziqli regressiya (linear regression) uchun model quramiz:
     const model = tf.sequential();
-    model.add(tf.layers.dense({units: 1, inputShape: [1]}));
-    
-    model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
-    
-    // Generate some synthetic data for training.
+    model.add(tf.layers.dense({ units: 1, inputShape: [1] }));
+
+    // modelimizni kompilyatsiya qilamiz:
+    model.compile({ loss: 'meanSquaredError', optimizer: 'sgd', metrics: 'accuracy' });
+
+    // Modelimizni o'qitish (training) uchun mo'ljallangan kiruvchi va natijaviy ma'lumot:
     const data = tf.tensor2d([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [11, 1]);
     const labels = tf.tensor2d([32, 33.8, 35.6, 37.4, 39.2, 41, 42.8, 44.6, 46.4, 48.2, 50], [11, 1]);
-    
-    // Train the model using the data.
-    const modelResult = await model.fit(data, labels, {epochs: 1200});
-    //console.log(modelResult.history.loss[0]);   
-    
+     
+    // Yuqoridagi ma'lumotni modelimizga kiritib, modelni o'qitamiz (training):
+    const modelResult = await model.fit(data, labels, {
+        epochs: 800, // davrlar soni - ya'ni modelni necha marta o'qitish soni
+        callbacks: { onEpochEnd: (epoch, logs) => console.log(logs.loss) } 
+        // har bir o'qitish davri tugaganida (onEpochEnd xodisasi) konsolga o'qitishning natijasidagi xatolar
+        // miqdori chiqariladi...e'tibor bering xato miqdori borgan sayin kamayib boradi.
+        }
+    );
+
+    // Endi kirishda 15 soni kiritilsa, natijada qanday son bo'lishini modelimiz bashorat qilib berishi kerak.
+    // Ya'ni, harorat tselziy bo'yicha 15 daraja bo'lganida, farengeytda u qancha bo'lishini modelimiz bizga aniqlab beradi:
     const predictionResult = model.predict(tf.tensor2d([15], [1, 1]));
-    //const result = model.predict(tf.tensor2d([15, 16, 17], [3, 1]));
     predictionResult.print();
 }
 
-GetPrediction();
+
+PredictCelciusToFarhrenheit();
